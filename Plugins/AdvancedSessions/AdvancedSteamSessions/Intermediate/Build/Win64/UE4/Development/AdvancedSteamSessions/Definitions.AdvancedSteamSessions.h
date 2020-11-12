00662db73bc44558e636caa99ac7fa51b370f986
David Li <lidavid@seas.upenn.edu>
2020-11-11 20:31:18 -0500

    done?

--- Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
+++ Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
@@ -33,8 +33,8 @@
 #define WITH_PUSH_MODEL 0
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
-#define UE_LIVE_CODING_ENGINE_DIR "C:\\Program Files\\Epic Games\\UE_4.25\\Engine"
-#define UE_LIVE_CODING_PROJECT "C:\\Users\\sharo\\UnrealProjects\\Rollio\\Rolling.uproject"
+#define UE_LIVE_CODING_ENGINE_DIR "D:\\EpicLibrary\\UE_4.25\\Engine"
+#define UE_LIVE_CODING_PROJECT "C:\\dev\\Unreal\\Rollio\\Rolling.uproject"
 #define WITH_XGE_CONTROLLER 0
 #define UBT_MODULE_MANIFEST "UE4.modules"
 #define UBT_MODULE_MANIFEST_DEBUGGAME "UE4-Win64-DebugGame.modules"
@@ -51,7 +51,7 @@
 #define RHI_RAYTRACING 1
 #define NDEBUG 1
 #define UE_BUILD_DEVELOPMENT 1
-#define UE_ENGINE_DIRECTORY "../../../../../../Program Files/Epic Games/UE_4.25/Engine/"
+#define UE_ENGINE_DIRECTORY "D:/EpicLibrary/UE_4.25/Engine/"
 #define UE_IS_ENGINE_MODULE 0
 #define UE_PROJECT_NAME Rolling
 #define UE_TARGET_NAME Rolling
